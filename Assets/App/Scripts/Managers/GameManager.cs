456067c89487490111bad09d9edf0da745fd69d7
Miroslav Stetsiuk <miroslavshard@gmail.com>
2020-09-11 14:08:48 +0300

    Initial commit

---
+++ Assets/App/Scripts/Managers/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public string secret;
+    public List<Guess> guesses = new List<Guess>();
+
+    #region Singleton activation
+    [HideInInspector] public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    #endregion
+
+    private void Start()
+    {
+        CheckGuess("4213");
+    }
+
+    public void CheckGuess(string guess)
+    {
+        guesses.Add(new Guess());
+        guesses.Last().guess = guess;
+
+        for (int guessChar = 0; guessChar < 4; guessChar++)
+        {
+            for (int secretChar = 0; secretChar < 4; secretChar++)
+            {
+                if (guesses.Last().guess[guessChar] == secret[secretChar])
+                {
+                    if (guessChar == secretChar) 
+                    {
+                        guesses.Last().bulls += 1;
+                    }
+                    else
+                    {
+                        guesses.Last().cows += 1;
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class Guess
+{
+    public string guess;
+    [Space(5)]
+
+    public int bulls = 0;
+    public int cows = 0;
+}
---
+++ Assets/App/Scripts/Managers/InputManager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+    public InputField[] numbers;
+    public Button[] buttons;
+
+    #region Singleton activation
+    [HideInInspector] public static InputManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    #endregion
+
+    public void OnButtonClick(string buttonID)
+    {
+        switch (buttonID)
+        {
+            case "1":
+                AddNumber(1);
+                break;
+
+            case "2":
+                AddNumber(2);
+                break;
+
+            case "3":
+                AddNumber(3);
+                break;
+
+            case "4":
+                AddNumber(4);
+                break;
+
+            case "5":
+                AddNumber(5);
+                break;
+
+            case "6":
+                AddNumber(6);
+                break;
+
+            case "7":
+                AddNumber(7);
+                break;
+
+            case "8":
+                AddNumber(8);
+                break;
+
+            case "9":
+                AddNumber(9);
+                break;
+
+            case "0":
+                AddNumber(0);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public void AddNumber(int number)
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i].text == "")
+            {
+                numbers[i].text = number.ToString();
+                buttons[number].interactable = false;
+                break;
+            }
+        }
+    }
+
+    public void RemoveNumber(int number)
+    {
+
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
